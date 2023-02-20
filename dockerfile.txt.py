89d05a944d56fe7ec1bc6c373b46f3549c591ff8
Maxxwell <maximetokoudeepness@gmail.com>
2023-02-20 22:46:51 +0000

    correct the name of the watch_next.py file

--- dockerfile.txt.py
+++ dockerfile.txt.py
@@ -10,4 +10,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 CMD ["python", "watch_next.py"]
 
-# for more informations visit https://docs.docker.com/develop/develop-images/dockerfile_best-practices/
+# for more informations visit https://docs.docker.com/develop/develop-images/dockerfile_best-practices/ 
